a439114a234d684a9f6de66a14b8e1b2fb6127c3
dcstylexf <dcstylexf@gmail.com>
2024-04-06 01:34:09 +0700

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const InputScreen = () => {
+    const [ inputValue, setInputValue ] = useState('');
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        let formData = new FormData(e.target);
+
+        setInputValue(formData.get('inputValue'));
+    }
+
+    return (
+        <div style={{ margin: '15em 20em', display: 'flex', flexDirection: 'column' }}>
+            <div style={{ margin: '1em auto', fontSize: '18pt' }}>
+                The input value is <b>{inputValue ? inputValue : 'undefined'}</b>
+            </div>
+
+            <form
+                onSubmit={handleFormSubmit}
+                style={{margin: '1em auto', display: 'flex', flexDirection: 'column'}}>
+                <input name="inputValue"
+                       type='text'
+                       placeholder='Enter something...'
+                       style={{padding: '.5rem 1rem', fontSize: '12pt'}}
+                />
+
+                <button type='submit' value='submit'
+                        style={{marginTop: '1rem', padding: '.5rem 0', fontSize: '12pt'}}>
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<InputScreen/>);
